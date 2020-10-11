f305d5a7fd92f0598092b2d5e3c82f02b6a33c90
Lovedeep ViRk <72649235+xdeep-burg@users.noreply.github.com>
2020-10-11 11:47:52 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,13 +29,11 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [DarkShadow ViRk](t.me/Darkshadow_officialy)
 
 Special thanks to [this person](t.me/denver02) for helping me.
 
 Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
