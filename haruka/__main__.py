61276d79b15039fab462864766f933023485ec3a
Lovedeep ViRk <72649235+xdeep-burg@users.noreply.github.com>
2020-10-17 17:19:30 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,11 +29,11 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Owner [DarkShadow ViRk](t.me/Darkshadow_officialy)
+Owner [Lovedeep ViRk](t.me/Darkshadow_officialy)
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/DarkShadowManager_Bot?startgroup=true).
 """
 
 
